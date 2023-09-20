a051eef0f05362b4f3821500e5eabe0623b6166f
aditeyapandey <aditeyapandey@gmail.com>
2023-09-19 21:51:11 -0400

    Updating code in app.ts

--- src/App.ts
+++ src/App.ts
@@ -1,7 +1,18 @@
-import { User } from "./User";
+import { Chart } from "./chart";
+import * as $ from 'jquery';
 
-window.onload = () => {
-  let title = document.querySelector("#title");
-  const tsUser = new User("Dany Paredes", 36);
-  if (title) title.innerHTML = tsUser.hello();
+window.onload = async() => {
+  const chart = new Chart();
+    let chartData = await chart.getData();
+    console.log(chartData);
+    // @ts-ignore
+    let sankey = chart.sankeyChart({
+      links: chartData,
+    },{
+      nodeGroup: function(d){
+        return d.id.split(/\W/)[0]}, // take first word for color
+    })
+    console.log(sankey);
+    //embed chart using jquery
+    $("#chart").html(sankey);
 };
